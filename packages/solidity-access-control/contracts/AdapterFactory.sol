878e36945b1b61f9a1bd169aef87abd59c50e565
vgorin <basil.gorin@gmail.com>
2023-10-09 23:28:16 +0300

    [OwnableToAccessControl]AdapterFactory added

---
+++ packages/solidity-access-control/contracts/AdapterFactory.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2;
+
+import "./OwnableToAccessControlAdapter.sol";
+
+/**
+ * @notice Ownable is a contract which is aware of its owner, that is has owner() function
+ */
+interface Ownable {
+	/**
+	 * @notice Smart contract owner
+	 *
+	 * @return the address of the smart contract owner
+	 */
+	function owner() external returns(address);
+}
+
+/**
+ * @title Adapter Factory
+ *
+ * @notice Helper contract simplifying the deployment of the OwnableToAccessControlAdapter
+ *
+ * @author Basil Gorin
+ */
+contract AdapterFactory {
+	/**
+	 * @dev Fired in deployNewOwnableToAccessControlAdapter
+	 *
+	 * @param adapterAddress newly deployed OwnableToAccessControlAdapter address
+	 * @param ownableTargetAddress OZ Ownable target contract address
+	 */
+	event NewOwnableToAccessControlAdapterDeployed(address indexed adapterAddress, address indexed ownableTargetAddress);
+
+	/**
+	 * @notice Deploys new OwnableToAccessControlAdapter bound to the OZ Ownable contract specified.
+	 *      Can be executed only by the OZ Ownable target owner. This owner is expected to transfer
+	 *      the ownership to the newly deployed OwnableToAccessControlAdapter contract address.
+	 *
+	 * @param targetAddress OZ Ownable target address to bind OwnableToAccessControlAdapter to
+	 * @return address of the newly deployed OwnableToAccessControlAdapter contract
+	 */
+	function deployNewOwnableToAccessControlAdapter(address targetAddress) public returns(address) {
+		// verify sender is a target owner
+		require(Ownable(targetAddress).owner() == msg.sender, "not an owner");
+
+		// deploy the OwnableToAccessControlAdapter
+		address adapterAddress = address(new OwnableToAccessControlAdapter(targetAddress));
+
+		// emit an event
+		emit NewOwnableToAccessControlAdapterDeployed(adapterAddress, targetAddress);
+
+		// return address of the newly deployed OwnableToAccessControlAdapter contract
+		return adapterAddress;
+	}
+}
--- packages/solidity-access-control/deploy/deploy-AdapterFactory.js
+++ packages/solidity-access-control/deploy/deploy-AdapterFactory.js
@@ -1,18 +1,13 @@
-// deploy: npx hardhat deploy --network goerli --tags OwnableToAccessControlAdapter
+// deploy: npx hardhat deploy --network goerli --tags AdapterFactory
 // verify: npx hardhat etherscan-verify --network goerli --api-key $ETHERSCAN_KEY
 
 // script is built for hardhat-deploy plugin:
 // A Hardhat Plugin For Replicable Deployments And Easy Testing
 // https://www.npmjs.com/package/hardhat-deploy
 
-// BN utils
-const {
-	toBN,
-	print_amt,
-} = require("../scripts/include/bn_utils");
-
 // deployment utils (contract state printers)
 const {
+	print_amt,
 	print_contract_details,
 } = require("../scripts/deployment_utils");
 
@@ -31,20 +26,16 @@ module.exports = async function({deployments, getChainId, getNamedAccounts, getU
 	console.log("network %o %o", chainId, network.name);
 	console.log("accounts: %o, service account %o, nonce: %o, balance: %o ETH", accounts.length, A0, nonce, print_amt(balance));
 
-	// TetherToken (OZ Ownable)
+	// AdapterFactory
 	{
-		// get target deployment details
-		const target_deployment = await deployments.get("TetherToken");
-		const target_contract = new web3.eth.Contract(target_deployment.abi, target_deployment.address);
-
 		// deploy if required
-		await deployments.deploy("OwnableToAccessControlAdapter", {
+		await deployments.deploy("AdapterFactory", {
 			// address (or private key) that will perform the transaction.
 			// you can use `getNamedAccounts` to retrieve the address you want by name.
 			from: A0,
-			contract: "OwnableToAccessControlAdapter",
+			contract: "AdapterFactory",
 			// the list of argument for the constructor (or the upgrade function in case of proxy)
-			args: [target_deployment.address],
+			// args: [],
 			// if set it to true, will not attempt to deploy even if the contract deployed under the same name is different
 			skipIfAlreadyDeployed: true,
 			// if true, it will log the result of the deployment (tx hash, address and gas used)
@@ -52,7 +43,7 @@ module.exports = async function({deployments, getChainId, getNamedAccounts, getU
 		});
 
 		// get the deployment details
-		const deployment = await deployments.get("OwnableToAccessControlAdapter");
+		const deployment = await deployments.get("AdapterFactory");
 		const contract = new web3.eth.Contract(deployment.abi, deployment.address);
 
 		// print the deployment details
@@ -65,5 +56,4 @@ module.exports = async function({deployments, getChainId, getNamedAccounts, getU
 // Then if another deploy script has such tag as a dependency, then when the latter deploy script has a specific tag
 // and that tag is requested, the dependency will be executed first.
 // https://www.npmjs.com/package/hardhat-deploy#deploy-scripts-tags-and-dependencies
-module.exports.tags = ["OwnableToAccessControlAdapter", "deploy"];
-module.exports.dependencies = ["TetherToken"];
+module.exports.tags = ["AdapterFactory", "deploy"];
